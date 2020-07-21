783803eed1f2d2b33e901a9a19566dcfb4b64fdb
Thugday <anais41721@naver.com>
2020-07-21 16:58:30 +0900

    add

---
+++ 10821.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int count(0);
+	string s;
+
+	cin >> s;
+
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == ',') { //comma 갯수를 확인 해 주면 몇 개의 정수인지 확인할 수 있음.
+			count++;
+		}
+	}
+	cout << count + 1 << '\n'; // 숫자는 항상 comma 보다 하나가 많기 때문에 +1을 해줌.
+	return 0;
+}
