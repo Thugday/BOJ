4cad434e1049806638ade0f5d051993a8085913f
Thugday <anais41721@naver.com>
2020-07-21 17:20:20 +0900

    add

---
+++ 10823.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	string s;
+	string line;
+	while (cin >> line) {
+		s += line;
+	}
+
+	istringstream sin(s); //문자열을 표준입출력처럼 사용하기 위해 istringstream 사용.
+
+	string num;
+	int sum(0);
+
+	while (getline(sin, num, ',')) {
+		sum += stoi(num);
+	}
+
+	cout << sum << '\n';
+	return 0;
+}
