28e7135e02162ac3925cd4f493bb66b973f18cf3
Thugday <anais41721@naver.com>
2020-07-21 14:56:11 +0900

    add

---
+++ 10867.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	set<int> s;
+
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		s.insert(x); //set에 값 저장.
+	}
+
+	for (auto a : s) {
+		cout << a << ' '; //set의 값을 모두 출력.
+	}
+
+	cout << '\n';
+
+	return 0;
+}
