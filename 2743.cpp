6ba585dc8cd6c9e3ae39becb71cf0a333d2fd200
Thugday <anais41721@naver.com>
2020-07-21 17:02:25 +0900

    add

---
+++ 2743.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string s;
+	cin >> s;
+
+	cout << s.length() << '\n'; // length를 사용 해 단어길이 재주기.
+
+	return 0;
+}
