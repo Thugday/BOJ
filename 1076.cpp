9db356a1146c1d11d71489f3de17d904d9e96ee9
Thugday <anais41721@naver.com>
2020-07-21 15:57:02 +0900

    add

---
+++ 1076.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	map<string, int> m = { {"black", 0}, {"brown", 1}, {"red", 2}, {"orange", 3}, {"yellow", 4}, {"green", 5}, {"blue", 6}, {"violet", 7},
+							{"grey", 8}, {"white", 9} }; // string을 정수로 변환시켜주기 위해 map사용.
+	string x, y, z;
+	cin >> x >> y >> z;
+
+	long long num = (long long)((m[x] * 10) + m[y]);
+	for (int i = 0; i < m[z]; i++) {
+		num *= 10LL;
+	}
+
+	cout << num << '\n';
+
+	return 0;
+}
