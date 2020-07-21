768ae0bc2b32ad74e052aba4ae66437ac3355556
Thugday <anais41721@naver.com>
2020-07-21 16:55:27 +0900

    add

---
+++ 10820.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int small(0), big(0), num(0), space(0);
+	string s;
+
+	while (getline(cin, s)) { //getline 을 사용해 한 줄을 모두 입력.
+		for (char x : s) {
+			if (x >= 'a' && x <= 'z') { // 소문자
+				small += 1;
+			}
+			else if ( x >= 'A' && x <= 'Z'){ // 대문자
+				big += 1;
+			}
+			else if (x >= '0' && x <= '9') { // 숫자
+				num += 1;
+			}
+			else if (x == ' ') { //공백
+				space += 1;
+			}
+		}
+		cout << small << " " << big << " " << num << " " << space;
+	}
+	return(0);
+}
