117d56b6f881ed00fc9362744799989834c0298c
Thugday <anais41721@naver.com>
2020-07-21 16:46:09 +0900

    add

---
+++ 12813.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <bitset>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string s1, s2; 
+	cin >> s1 >> s2; //문자열로 bit string을 입력 받음.
+
+	bitset<100000> b1(s1), b2(s2);
+	cout << (b1 & b2) << '\n';
+	cout << (b1 | b2) << '\n';
+	cout << (b1 ^ b2) << '\n';
+	cout << (~b1) << '\n';
+	cout << (~b2) << '\n';
+
+	return 0;
+}
