aac34c854e07aea72d04a2a87f3e457b3a8fb9ec
Thugday <anais41721@naver.com>
2020-07-22 16:52:18 +0900

    add

---
+++ 10818.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+
+	cin >> n;
+
+	vector<int> a(n);
+	
+	for (int i = 0; i < n; i++) {
+		cin >> a[i]; // 주어진 정수 input 해주기.
+	}
+
+	auto it1 = min_element(a.begin(), a.end());
+	cout << *it1 << ' '; 
+	auto it2 = max_element(a.begin(), a.end());
+	cout << *it2;
+
+	return 0;
+}
