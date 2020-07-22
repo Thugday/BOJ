c03be762367fa727a0df7496d4cf0039054bf7d1
Thugday <anais41721@naver.com>
2020-07-22 16:14:42 +0900

    add

---
+++ 10814.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Person {
+	int age;
+	string name;
+}; // 인물의 나이 이름 생성.
+
+bool cmp(const Person &u, const Person &v) {
+	if (u.age < v.age) {
+		return true;
+	}
+	else {
+		return false;
+	}
+} // 나이 비교를 위한 비교함수.
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+
+	cin >> n;
+	vector<Person> m(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> m[i].age >> m[i].name;
+	}
+
+	stable_sort(m.begin(), m.end(), cmp); //stable_sort를 사용함으로써 원 상태 유지 하면서 비교
+
+	for (int i = 0; i < n; i++) {
+		cout << m[i].age << ' ' << m[i].name << "\n";
+	}
+
+	return 0;
+}
