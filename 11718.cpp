6e3bf643a766e099bea89c187760ffb25b5b5425
Thugday <anais41721@naver.com>
2020-07-23 17:52:01 +0900

    add

---
+++ 11718.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string s;
+
+	while (true) {
+		getline(cin, s); // 한 줄 모두 입력 받기.
+		if (s == "") {
+			break;
+		}
+		cout << s << "\n";
+	}
+
+	return 0;
+}
