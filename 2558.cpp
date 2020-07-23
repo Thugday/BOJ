1e045f3ca98be909c7f6f914cfc20cb1d9cd04c4
Thugday <anais41721@naver.com>
2020-07-23 16:14:36 +0900

    add

---
+++ 2558.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+	int m; 
+	cin >> m;
+
+	int sum = n + m;
+	cout << sum << '\n';
+
+	return 0;
+}
