166c4cc3dfbbd255f2be55a52484d87466d4465e
Thugday <anais41721@naver.com>
2020-07-24 14:31:27 +0900

    add

---
+++ 2742.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	for (int i = n; i > 0; i--) { //감소하는 순으로 정렬
+		cout << i << '\n';
+	}
+
+	return 0;
+}
