ff17439d1cf08d5b108aeecfbf7ce0f65edec34c
Thugday <anais41721@naver.com>
2020-07-24 15:37:33 +0900

    add

---
+++ 2445.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= i; j++) {
+			cout << "*";
+		}
+		for (int k = 1; k <= 2 * (n - i); k++) {
+			cout << " ";
+		}
+		for (int j = 1; j <= i; j++) {
+			cout << "*";
+		}
+		cout << '\n';
+	}
+
+	for (int i = 1; i <= n - 1; i++) {
+		for (int j = i; j < n; j++) {
+			cout << "*";
+		}
+		for (int k = 1; k <= 2 * i; k++) {
+			cout << " ";
+		}
+		for (int j = i; j < n; j++) {
+			cout << "*";
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
