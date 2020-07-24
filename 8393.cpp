c3432a70f37fa4121c2fb89bfcacf823ccac7ffc
Thugday <anais41721@naver.com>
2020-07-24 14:43:47 +0900

    add

---
+++ 8393.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	int sum(0);
+	cin >> n;
+
+	for (int i = 0; i <= n; i++) {
+		sum += i; // n까지 계속 더해주기.
+	}
+
+	cout << sum;
+
+	return 0;
+}
