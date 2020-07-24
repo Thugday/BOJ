c3e966391424f3bc3f5ade6124d02ec8e9ff77a1
Thugday <anais41721@naver.com>
2020-07-24 18:53:30 +0900

    add

---
+++ 10799.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string str;
+	cin >> str;
+
+	int n = str.size();
+	stack<int> s;
+	int ans = 0;
+
+	for (int i = 0; i < n; i++) {
+		if (str[i] == '(') {
+			s.push(i);
+		}
+		else {
+			if (s.top() + 1 == i) { // 레이져 일때 스택의 크기 만큼 더해준다.
+				s.pop();
+				ans += s.size();
+			}
+			else {
+				s.pop(); // 레이져가 아닐때는 쇠막대기이기 때문에 1을 더해준다.
+				ans += 1; 
+			}
+		}
+	}
+	cout << ans << '\n';
+
+	return 0;
+}
