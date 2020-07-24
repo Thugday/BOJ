95408d5dee2ae88cc9a064797e37056fa6aece73
Thugday <anais41721@naver.com>
2020-07-24 15:20:22 +0900

    add

---
+++ 2442.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n - i - 1; j++) { // 공백 부터 출력 해줘서 대칭 만들기
+			cout << " "; 
+		}
+		for (int j = 0; j < 2 * i + 1; j++) { // 2*n+1 찍어주기.
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
