65c83996a0d7b9b2f48af4d7c2198d03acf57610
Thugday <anais41721@naver.com>
2020-07-24 15:05:23 +0900

    add

---
+++ 2441.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, j;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (j = 0; j < i; j++) { // 공백은 증가 순
+			cout << " ";
+		}
+		for (j = n; j > i; j--) { // *은 감소 순
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
