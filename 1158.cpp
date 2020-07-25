48f7c18e46b2318953aae246b58dd3aaecea8ac3
Thugday <anais41721@naver.com>
2020-07-25 16:18:00 +0900

    add

---
+++ 1158.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    queue<int> q;
+    for (int i = 1; i <= n; i++) {
+        q.push(i); // 큐에 value 저장하기.
+    }
+
+    cout << "<";
+    
+    for (int i = 0; i < n - 1; i++) { // n만큼 실행
+        for (int j = 0; j < m - 1; j++) { // m만큼 실행.
+            q.push(q.front()); // 맨 앞 value를 뒤로 push.
+            q.pop(); // 맨 앞 pop. 
+        }
+        cout << q.front() << ", ";
+        q.pop(); // 모두 돌아갔을 시 맨 앞 pop.
+    }
+    cout << q.front() <<">" << '\n';
+    return 0;
+}
