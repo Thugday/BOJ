5a1eb7ec731ea5769e937b7ae9bb8078c1895e12
Thugday <anais41721@naver.com>
2020-07-27 16:52:36 +0900

    add

---
+++ 10808.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string s;
+
+	cin >> s; // 단어 입력 받기.
+
+	for (int i = 'a'; i <= 'z'; i++) {
+		cout << count(s.begin(), s.end(), i) << ' ';
+	} // 소문자로 이루어진 단어의 알파벳 갯수 찾아주기.
+	
+	cout << '\n';
+
+	return 0;
+}
