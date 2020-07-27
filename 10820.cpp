a036dcb29b4aa0ca334f36b9c6785897e8c20a97
Thugday <anais41721@naver.com>
2020-07-27 17:55:14 +0900

    edit

--- 10820.cpp
+++ 10820.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,25 +8,27 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int small(0), big(0), num(0), space(0);
 	string s;
-
-	while (getline(cin, s)) { //getline 을 사용해 한 줄을 모두 입력.
+	
+	
+	while (getline(cin, s)) { // 한 줄 입력 받기.
+		int small(0), big(0), space(0), num(0);
 		for (char x : s) {
-			if (x >= 'a' && x <= 'z') { // 소문자
-				small += 1;
+			if (x >= 'a' && x <= 'z') {
+				small += 1;	// 소문자 찾기 및 소문자 수량 증가
 			}
-			else if ( x >= 'A' && x <= 'Z'){ // 대문자
-				big += 1;
+			else if (x >= 'A' && x <= 'Z') {
+				big += 1; // 대문자 찾기 및 대문자 수량 증가
 			}
-			else if (x >= '0' && x <= '9') { // 숫자
-				num += 1;
+			else if (x >= '0' && x <= '9') {
+				num += 1; // 숫자 찾기 및 숫자 수량 증가
 			}
-			else if (x == ' ') { //공백
-				space += 1;
+			else if (x == ' ') {
+				space += 1; // 공백 찾기 및 공백 수량 증가
 			}
 		}
-		cout << small << " " << big << " " << num << " " << space;
+		cout << small << ' ' << big << ' ' << num << ' ' << space << '\n'; 
 	}
-	return(0);
+
+	return 0;
 }
