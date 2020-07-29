44dd30cc33b2d3f14ac4ed09c8e9e99944db0d0b
Thugday <anais41721@naver.com>
2020-07-29 16:26:16 +0900

    edit

--- 11052.cpp
+++ 11052.cpp
@@ -19,7 +19,7 @@ int main() {
 	vector<int> b(n + 1);
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= i; j++) {
-			b[i] = max(b[i], b[i - j] + a[j]); // i-j까지 p 값을 더해주며 max값을 구해줌.
+			b[i] = max(b[i], b[i - j] + a[j]); // i-j까지 a 값을 더해주며 max값을 구해줌.
 		}
 	}
 
