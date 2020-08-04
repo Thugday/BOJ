5f9d9f77d8437c6c08e6cc9d700a96b8ae1d4b5e
Thugday <anais41721@naver.com>
2020-08-04 17:36:14 +0900

    add

---
+++ 2745.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n(0), b;
+	
+	string s = "";
+
+	cin >> s >> b;
+	
+	for (int i = 0; i < s.size(); i++) {
+		if ('0' <= s[i] && s[i] <= '9') {
+			n = n * b + (s[i] - '0'); // n의 수를 b진법이니 b로 곱해주면서 size만큼 계속 더해준다.
+		}
+		else {
+			n = n * b + (s[i] - 'A' + 10);
+		}
+	}
+
+	cout << n << '\n';
+
+	return 0;
+}
