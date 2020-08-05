48162457e190bb0fa09bd59f337f4115780ea540
Thugday <anais41721@naver.com>
2020-08-05 17:37:53 +0900

    add

---
+++ 10872.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// 팩토리얼 구해주기.
+int getFactorial(int n) {
+	if (n == 0) {
+		return 1;
+	}
+	else {
+		return n * getFactorial(n - 1);
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+	getFactorial(n);
+
+	cout << getFactorial(n) << '\n';
+
+	return 0;
+}
