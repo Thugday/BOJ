980d67246c1f151e77b7dd18d093523c0fe40bb4
Thugday <anais41721@naver.com>
2020-08-12 14:43:27 +0900

    add

---
+++ 1992.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <string>
+#define MAX 65
+
+using namespace std;
+
+int d[MAX][MAX];
+
+void solve(int n, int y, int x) {
+	if (n == 1) {
+		cout << d[y][x];
+		return;
+	}
+
+	bool zero = true, one = true;
+
+	for (int i = y; i < y + n; i++) {
+		for (int j = x; j < x + n; j++) {
+			if (d[i][j]) {
+				zero = false;
+			}
+			else {
+				one = false;
+			}
+		}
+	}
+
+	if (zero) {
+		cout << 0;
+	}
+	else if (one) {
+		cout << 1;
+	}
+	else {
+		// 4개의 영역을 나눠서 다시 solve 함수에 넣어준다.
+		cout << "(";
+		solve(n / 2, y, x);
+		solve(n / 2, y, x + n / 2);
+		solve(n / 2, y + n / 2, x);
+		solve(n / 2, y + n / 2, x + n / 2);
+		cout << ")";
+	}
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	bool zero = true;
+	bool one = true;
+
+	for (int i = 0; i < n; i++) {
+		string s; // string으로 입력 받기.
+		cin >> s;
+
+		for (int j = 0; j < n; j++) {
+			d[i][j] = s[j] - '0';
+		}
+	}
+
+	solve(n, 0, 0);
+	cout << '\n';
+
+	return 0;
+}
---
+++ 2263.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int inorder[100010];
+int postorder[100010];
+int preorder[100010];
+
+// 인오더, 포스트오더의 시작점과 끝점을 사용한 solve 함수. 포스트오더의 끝점은 루트이다.
+void solve(int in_start, int in_end, int post_start, int post_end) {
+	if (in_start > in_end || post_start > post_end) { // 시작점이 끝점보다 작거나 같지 않으면 return.
+		return;
+	}
+	int root = postorder[post_end];
+	cout << root << ' ';
+	
+	int p = preorder[root];
+	int lhs = p - in_start;
+	
+	solve(in_start, p - 1, post_start, post_start + lhs - 1); // 왼쪽 트리 찾기.
+	solve(p + 1, in_end, post_start + lhs, post_end - 1); // 오른쪽 트리 찾기.
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> inorder[i];
+	}
+	
+	for (int i = 0; i < n; i++) {
+		cin >> postorder[i];
+	}
+	
+	for (int i = 0; i < n; i++) {
+		preorder[inorder[i]] = i;
+	}
+
+	solve(0, n - 1, 0, n - 1);
+
+	return 0;
+}
