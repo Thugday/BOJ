aae3da87a484e0677aed0742590b666ac6d74451
Thugday <anais41721@naver.com>
2020-08-17 17:49:21 +0900

    add

---
+++ 10974.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	vector<int> v(n);
+	for (int i = 0; i < n; i++) {
+		v[i] = i + 1;
+	}
+
+	// do while 을 사용하여 모든 순열을 구해준다.
+	do {
+		for (int i = 0; i < n; i++) {
+			cout << v[i] << ' ';
+		}
+		cout << '\n';
+	} while (next_permutation(v.begin(), v.end()));
+
+	return 0;
+}
